2c32a66d0eed6ede7876af90f65e324616635804
annstriganova <20650874+annstriganova@users.noreply.github.com>
2024-07-29 00:09:01 +0200

    Added @PostConstruct for Stripe.apikey set up

--- src/main/java/com/zufar/icedlatte/IcedLatteApplication.java
+++ src/main/java/com/zufar/icedlatte/IcedLatteApplication.java
@@ -1,14 +1,11 @@
 package com.zufar.icedlatte;
 
-import com.zufar.icedlatte.payment.config.StripeConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableConfigurationProperties(StripeConfiguration.class)
 @EnableScheduling
 @EnableCaching
 public class IcedLatteApplication {
