67fab9d816bc0a1d8b39ce0397fe8357c89bdeb8
zufar-sunagatov <zufar.sunagatov@gmail.com>
2024-07-29 00:00:19 +0100

    PaymentEndpoint fix

--- src/main/java/com/zufar/icedlatte/payment/endpoint/PaymentEndpoint.java
+++ src/main/java/com/zufar/icedlatte/payment/endpoint/PaymentEndpoint.java
@@ -22,8 +22,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-import java.util.UUID;
-
 @Slf4j
 @RequiredArgsConstructor
 @RestController
